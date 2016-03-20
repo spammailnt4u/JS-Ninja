fa5fcba50c1efe1664cf80690732ef599b1fac50
spammailnt4u <spammailnt4u@gmail.com>
2016-03-20 19:12:16 -0400

    Added ch 2 code with quiz prompt

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,5 @@
 //welcome the user
-alert("Welcome to Quiz Ninja!");
+alert('Welcome to Quiz Ninja!');
+var question = 'What is Superman\'s real name?';
+var answer = prompt(question);
+alert('You just answered ' + answer);
