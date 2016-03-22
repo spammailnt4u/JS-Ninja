fd4306fb2a07d5bc702c461ede66856f8f2c6577
spammailnt4u <spammailnt4u@gmail.com>
2016-03-22 18:27:34 -0400

    CH3 Added 3 questions in a prompt

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
 //welcome the user
 alert('Welcome to Quiz Ninja!');
-var question = 'What is Superman\'s real name?';
-var answer = prompt(question);
-alert('You just answered ' + answer);
+
+var quiz = [
+  ['What is Superman\'s real name?','Clarke Kent'],
+  ['What is Wonderwoman\'s real name?','Dianna Prince'],
+  ['What is Batman\'s real name?','Bruce Wayne']
+];
+var score = 0; //initializes score
+for (var i=0,length=quiz.length;i<length;i++) {
+  var answer = prompt(quiz [i] [0]);
+  if (answer === quiz [i] [1]) {
+    alert('Correct!');
+    score++;
+  }
+  else {
+    alert('Incorrect')
+  }
+}
+alert('Your score is '+score);
