8d1ce82e17047eff49cfeb399f99093e5bd7d31f
spammailnt4u <spammailnt4u@gmail.com>
2016-03-22 22:22:53 -0400

    Added mean() and sort() functions

--- chp4/js/script.js
+++ chp4/js/script.js
@@ -2,3 +2,16 @@ function compareNumbers(a,b) {
   return a-b; //used to fix .sort() for #s
 }
 console.log('Use .sort(compareNumbers) to sort numbers')
+function mean(values, callback) {
+  var total=0;
+  for (var i=0, max = values.length; i < max; i++) {
+    if (typeof callback === 'function') {
+      total += callback(values[i]);
+    }
+    else {
+      total += values[i]
+    }
+  }
+  return total/max;
+}
+console.log('Use mean([#,#,#],function(x){return 2*x}) to return the mean of some numbers*2')
