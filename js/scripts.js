524bd804ffa0c795882e3b7cb3b2362b80a6c978
spammailnt4u <spammailnt4u@gmail.com>
2016-03-27 22:55:40 -0400

    ch4 Made quizninja code more readable

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,35 @@
-//welcome the user
-alert('Welcome to Quiz Ninja!');
-
 var quiz = [
   ['What is Superman\'s real name?','Clarke Kent'],
   ['What is Wonderwoman\'s real name?','Dianna Prince'],
   ['What is Batman\'s real name?','Bruce Wayne']
 ];
 var score = 0; //initializes score
-for (var i=0,length=quiz.length;i<length;i++) {
-  var answer = prompt(quiz [i] [0]);
-  if (answer === quiz [i] [1]) {
-    alert('Correct!');
-    score++;
+
+play(quiz);//whole function is hoisted
+
+function play(quiz) {
+  //main game loop
+  for (var i=0, question, answer, max=quiz.length; i<max; i++) {
+    question = quiz [i] [0];
+    answer = ask(question);
+    check(answer);
   }
-  else {
-    alert('Incorrect')
+  // end of main game loop
+  gameOver();
+  //defining necessary functions
+  function ask(question) {
+    return prompt(question);
+  }
+  function check(answer) {
+    if (answer === quiz[i][1]) {
+      alert('Correct!');
+      score++;
+    }
+    else {
+      alert('Incorrect!');
+    }
+  }
+  function gameOver() {
+    alert('Gamer over! You scored ' + score + ' points!')
   }
 }
-alert('Your score is '+score);
